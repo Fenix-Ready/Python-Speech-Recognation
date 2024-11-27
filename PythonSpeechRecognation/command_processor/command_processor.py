9cb750f518fcdbe2532f61c1ac367a355139ba21
Артём <94250279+Fenix-Ready@users.noreply.github.com>
2024-11-27 15:01:14 +0300

    Update command_processor.py

--- PythonSpeechRecognation/command_processor/command_processor.py
+++ PythonSpeechRecognation/command_processor/command_processor.py
@@ -1,35 +1,11 @@
 import webbrowser
-from langdetect import detect
-from translate import Translator
 
 
 class CommandProcessor:
-    def __init__(self):
-        self.translator = Translator(to_lang="en")
-
     def process_command(self, command):
         command = command.lower()
-        words = command.split()
-        language_results = []
+        print("Исходное предложение:", command)
 
-        for word in words:
-            try:
-                lang = detect(word)
-                if lang in ['en', 'ru']:
-                    language_results.append((word, lang))
-                else:
-                    print(f"Слово '{word}' не является английским или русским.")
-
-            except Exception as e:
-                print(f"Ошибка при определении языка для слова '{word}': {e}")
-                language_results.append((word, 'unknown'))
-
-        reconstructed_sentence = ' '.join([word for word, lang in language_results])
-        print("Определенные языки слов:", language_results)
-        print("Составленное предложение:", reconstructed_sentence)
-
-        translated_command = self.translator.translate(reconstructed_sentence)
-
-        if "открой google" in translated_command:
+        if "открой гугл" in command:
             webbrowser.open("https://www.google.com")
-            print("Открываю Google...")
+            # print("Открываю Google...")
